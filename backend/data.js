be1439dc6f92568138e7d796eab567819fa8280e
marvinlara41 <laramarvin105@gmail.com>
2021-02-02 17:13:57 -0500

    added a basic backend server

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Whey protein",
+      category: "protein",
+      image: "/images/proteins/whey2.jpg",
+      price: 40,
+      countInStock: 3,
+      brand: "ION",
+      rating: 4.5,
+      numReviews: 10,
+      description: "100% whey protein",
+    },
+    {
+      _id: "2",
+      name: "Whey protein",
+      category: "protein",
+      image: "/images/proteins/whey3.jpg",
+      price: 45,
+      countInStock: 6,
+      brand: "Carnivore",
+      rating: 5,
+      numReviews: 10,
+      description: "100% whey protein/ mass gainer",
+    },
+    {
+      _id: "4",
+      name: "Whey protein",
+      category: "protein",
+      image: "/images/proteins/whey5.jpg",
+      price: 45,
+      countInStock: 0,
+      brand: "Nutrition",
+      rating: 1.5,
+      numReviews: 10,
+      description: "100% whey protein",
+    },
+  ],
+};
+
+export default data;
