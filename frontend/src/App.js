8fc70147f3dc7a51c716cc30a486514cb7ea9c21
marvinlara41 <laramarvin105@gmail.com>
2021-02-02 16:01:09 -0500

    updated css to productScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
-import Product from "./components/Products";
-import data from "./data";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a href="/" className="brand">
-            Viribus
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product}></Product>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All rights resevered.</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a href="/" className="brand">
+              Viribus
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/" component={HomeScreen} exact />
+        </main>
+        <footer className="row center">All rights resevered.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
